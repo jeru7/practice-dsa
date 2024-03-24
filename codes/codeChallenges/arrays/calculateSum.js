843737cdc1e44c400da034cb3a2d655f594da13c
jeru7 <john.ungab7@gmail.com>
2024-03-24 21:51:30 +0800

    calculate sum code added

---
+++ codes/codeChallenges/arrays/calculateSum.js
@@ -0,0 +1,15 @@
+// Calculate the total Sum of Array Elements
+
+// Problem: Write a function that takes an array of numbers as input and returns the sum of all elements in the array.
+
+function calculateSum(arr) {
+  let total = 0
+
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i]
+  }
+
+  return total
+}
+
+console.log(calculateSum([2, 3, 4, 5]))
