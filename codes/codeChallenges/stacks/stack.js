e809d84ab4e0147796a80b86c54ff620a5e2a8ca
jeru7 <john.ungab7@gmail.com>
2024-03-30 14:51:58 +0800

    added the peek method for the stack

--- codes/codeChallenges/stacks/stack.js
+++ codes/codeChallenges/stacks/stack.js
@@ -17,6 +17,14 @@ class Stack {
     return this.items.pop()
   }
 
+  peek() {
+    if (this.isEmpty()) {
+      return 'Stack is empty'
+    }
+
+    return this.items[this.items.length - 1]
+  }
+
   isEmpty() {
     return this.items.length === 0
   }
