9cfcf6736791567dc0f9b46f59e8d6f7cc0ee0c5
jeru7 <john.ungab7@gmail.com>
2024-04-12 21:27:22 +0800

    binary tree implementation code added

---
+++ codes/codeChallenges/trees/binaryTree.js
@@ -0,0 +1,58 @@
+// Binary Tree Implementation using JavaScript
+
+// Binary Tree - A binary tree is a type of tree data structure in which each node has at most two children, referred to as the left child and the right child.
+
+class TreeNode {
+  constructor(value) {
+    this.value = value
+    this.right = null
+    this.left = null
+  }
+
+  isRoot() {
+    return !this.parent
+  }
+
+  isLeaf() {
+    return !this.right && !this.left
+  }
+
+  isInternal() {
+    return !this.isLeaf()
+  }
+
+  addChild(value) {
+    if (value <= this.value) {
+      if (!this.left) {
+        return (this.left = new TreeNode(value))
+      } else {
+        return this.left.addChild(value)
+      }
+    } else {
+      if (!this.right) {
+        return (this.right = new TreeNode(value))
+      } else {
+        return this.right.addChild(value)
+      }
+    }
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null
+  }
+
+  addRoot(value) {
+    if (this.root) {
+      console.log('Root already exists')
+      return
+    }
+    this.root = new TreeNode(value)
+    return this.root
+  }
+
+  isEmpty() {
+    return !this.root
+  }
+}
