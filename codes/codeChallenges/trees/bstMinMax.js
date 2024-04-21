aebd82ae8acc24d590c8a3982c9e71afb08a20f7
jeru7 <john.ungab7@gmail.com>
2024-04-21 15:13:20 +0800

    bst min and max method added

--- codes/codeChallenges/trees/bstMinMax.js
+++ codes/codeChallenges/trees/bstMinMax.js
@@ -7,3 +7,53 @@ class Node {
     this.right = null
   }
 }
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null
+  }
+
+  isEmpty() {
+    return !this.root
+  }
+
+  insert(value) {
+    let node = new Node(value)
+    if (!this.root) {
+      this.root = node
+    } else {
+      return this.insertNode(this.root, node)
+    }
+  }
+
+  insertNode(parent, child) {
+    if (child.value <= parent.value) {
+      if (!parent.left) {
+        parent.left = child
+      } else {
+        return this.insertNode(parent.left, child)
+      }
+    } else {
+      if (!parent.right) {
+        parent.right = child
+      } else {
+        return this.insertNode(parent.right, child)
+      }
+    }
+  }
+
+  min(node = this.root) {
+    if (node.left) {
+      return this.min(node.left)
+    }
+    return node.value
+  }
+
+  max(node = this.root) {
+    if (node.right) {
+      return this.max(node.right)
+    }
+
+    return node.value
+  }
+}
