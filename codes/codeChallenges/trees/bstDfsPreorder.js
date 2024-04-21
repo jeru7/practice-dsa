c243edbe627f7e2bf2393fd5fa00ec63eaffa48c
jeru7 <john.ungab7@gmail.com>
2024-04-21 10:38:12 +0800

    preorder dfs code added

---
+++ codes/codeChallenges/trees/bstDfsPreorder.js
@@ -0,0 +1,54 @@
+// Depth First Search (Preorder)
+// DFS: You start at the entrance (the root node) and keep going down a path until you reach a dead end (a leaf node), then you backtrack and try a different path.
+// Preorder DFS: The root node is visited first, then the left subtree is recursively explored, followed by the right subtree.
+
+class Node {
+  constructor(value) {
+    this.value = value
+    this.left = null
+    this.right = null
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null
+  }
+
+  isEmpty() {
+    return !this.root
+  }
+
+  insert(value) {
+    let node = new Node(value)
+    if (!this.root) {
+      this.root = node
+    } else {
+      return this.insertNode(this.root, node)
+    }
+  }
+
+  insertNode(parent, child) {
+    if (child.value <= parent.value) {
+      if (!parent.left) {
+        parent.left = child
+      } else {
+        return this.insertNode(parent.left, child)
+      }
+    } else {
+      if (!parent.right) {
+        parent.right = child
+      } else {
+        return this.insertNode(parent.right, child)
+      }
+    }
+  }
+
+  preorderDFS(node = this.root) {
+    if (node !== null) {
+      console.log(node.value)
+      this.preorderDFS(node.left)
+      this.preorderDFS(node.right)
+    }
+  }
+}
