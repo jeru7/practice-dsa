2423335c4f8b285b91fac949871f5449687743a0
jeru7 <john.ungab7@gmail.com>
2024-04-21 11:41:00 +0800

    dfs inorder code added

---
+++ codes/codeChallenges/trees/bfsDfsInorder.js
@@ -0,0 +1,64 @@
+// Depth First Search (Inorder)
+// DFS: You start at the entrance (the root node) and keep going down a path until you reach a dead end (a leaf node), then you backtrack and try a different path.
+// Inorder DFS: The left subtree is recursively explored first, then the root node is visited, and finally, the right subtree is explored.
+
+class Node {
+  constructor(value) {
+    this.value = value
+    this.left = null
+    this.right = null
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null
+  }
+
+  isEmpty() {
+    return !this.root
+  }
+
+  insert(value) {
+    let node = new Node(value)
+    if (!this.root) {
+      this.root = node
+    } else {
+      return this.insertNode(this.root, node)
+    }
+  }
+
+  insertNode(parent, child) {
+    if (child.value <= parent.value) {
+      if (!parent.left) {
+        parent.left = child
+      } else {
+        return this.insertNode(parent.left, child)
+      }
+    } else {
+      if (!parent.right) {
+        parent.right = child
+      } else {
+        return this.insertNode(parent.right, child)
+      }
+    }
+  }
+
+  inorderDFS(node = this.root) {
+    if (node) {
+      this.inorderDFS(node.left)
+      console.log(node.value)
+      this.inorderDFS(node.right)
+    }
+  }
+}
+
+let bst = new BinaryTree()
+
+bst.insert(10)
+bst.insert(5)
+bst.insert(15)
+bst.insert(3)
+bst.insert(7)
+
+bst.inorderDFS()
